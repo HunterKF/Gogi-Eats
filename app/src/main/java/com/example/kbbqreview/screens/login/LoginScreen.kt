c68f75447ecd69734dbb41104e53886dcffd5481
HunterKF <95839328+HunterKF@users.noreply.github.com>
2022-09-01 20:48:35 +0900

    Updates: - Adding a login screen. Currently FB sign in works. - Added Google to login screen. Gotta make it look pretty. - Moving most functions out of ApplicationViewModel.kt, and into their own individual view models. This makes things much smoother and more organized, I feel.
    To-Do:
    - Make proper sign in screen.
    - Fix bottom sheet. (Needs more functions and it should look better. Also should blur or shade the background)
    - Make camera look more functional.
    - Populate map with each location. Learn how to measure distance from current location. Oof.
    - Have to make a proper sign in and profile screen.
    - Make a user id for each profile. Also add in google sign in.
    
    Issues:
    - Permissions is still pretty weird.
    - App times out when trying to change screen from maps.
    - App sometimes dies when sliding through the images downloaded from firebase.
    - Firebase doesn't always load all the content, or at least, the main screen doesn't load it all right away.
    - Map is acting weird in emulator.
    - Profile bottom sheet doesn't look right. Need to add items and make it overlay the background.

--- app/src/main/java/com/example/kbbqreview/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/kbbqreview/screens/login/LoginScreen.kt
@@ -1,26 +1,36 @@
 package com.example.kbbqreview.screens.login
 
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Facebook
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.example.kbbqreview.R
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
@@ -33,6 +43,28 @@ fun LoginScreen(
     navigateToHome: () -> Unit
 ) {
     val context = LocalContext.current
+    val token = stringResource(R.string.default_web_client_id)
+    val gso =
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(token)
+            .requestEmail().build()
+    val googleSignInClient = GoogleSignIn.getClient(context, gso)
+
+    //not best practice?
+    var account: GoogleSignInAccount? by remember { mutableStateOf(null) }
+    //classic way?
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult() ) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+        try {
+            account = task.getResult(ApiException::class.java)
+            val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+            Firebase.auth.signInWithCredential(credential).addOnCompleteListener {
+                account = GoogleSignIn.getLastSignedInAccount(context)
+            }
+        } catch (e: ApiException) {
+            Log.w("Google", "Google sign in failed", e)
+        }
+    }
+
     Box(
         Modifier
             .background(MaterialTheme.colors.surface)
@@ -53,13 +85,16 @@ fun LoginScreen(
             Spacer(Modifier.height(20.dp))
             SignInButton(
                 onSignedIn = {
-                    navigateToHome
+                    navigateToHome()
                     Toast.makeText(context, "It signed in!", Toast.LENGTH_SHORT).show()
                 },
                 onSignInFailed = {
                     Toast.makeText(context, "Try again later.", Toast.LENGTH_SHORT).show()
                 }
             )
+            Button(onClick = { launcher.launch(googleSignInClient.signInIntent)}) {
+                Text("Google")
+            }
         }
     }
 }
@@ -68,8 +103,7 @@ fun LoginScreen(
 fun SignInButton(
     onSignInFailed: (Exception) -> Unit,
     onSignedIn: () -> Unit,
-
-    ) {
+) {
     val scope = rememberCoroutineScope()
     AndroidView(factory = { context ->
         LoginButton(context).apply {
--- app/src/main/java/com/example/kbbqreview/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/example/kbbqreview/screens/login/LoginViewModel.kt
@@ -1,4 +1,50 @@
 package com.example.kbbqreview.screens.login
 
+import android.content.Context
+import android.os.Bundle
+import android.provider.Settings.Global.getString
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import com.example.kbbqreview.R
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
+
 class LoginViewModel {
+    suspend fun signIn(
+        context: Context,
+        launcher: ActivityResultLauncher<IntentSenderRequest>
+    ) {
+        val oneTapClient = Identity.getSignInClient(context)
+        val signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(R.string.default_web_client_id.toString())
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build()
+            )
+            // Automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(true)
+            .build()
+
+        try {
+            // Use await() from https://kotlin.github.io/kotlinx.coroutines/kotlinx-coroutines-play-services
+            // Instead of listeners that aren't cleaned up automatically
+            val result = oneTapClient.beginSignIn(signInRequest).await()
+
+            // Now construct the IntentSenderRequest the launcher requires
+            val intentSenderRequest = IntentSenderRequest.Builder(result.pendingIntent).build()
+            launcher.launch(intentSenderRequest)
+        } catch (e: Exception) {
+            // No saved credentials found. Launch the One Tap sign-up flow, or
+            // do nothing and continue presenting the signed-out UI.
+            Log.d("LOG", e.message.toString())
+        }
+    }
 }
